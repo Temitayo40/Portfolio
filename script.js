afc3f19294b419e7ad2772af6098bfbcafdfbe78
Adebowale <adebowaletemitayo40@gmail.com>
2023-03-03 23:13:20 +0100

    implemented the reveal element on scroll features using javasript

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ const section1 = document.querySelector("#section--1");
 
 const obsCallback = function (entries, observer) {
   entries.forEach((entry) => {
-    console.log(entry);
     if (!entry.isIntersecting) headerNav.classList.add("sticky");
     else headerNav.classList.remove("sticky");
   });
@@ -22,3 +21,23 @@ const obsOptions = {
 
 const observer = new IntersectionObserver(obsCallback, obsOptions);
 observer.observe(header);
+
+///////REVEAL ON VIEW
+
+const allSections = document.querySelectorAll(".section");
+const revealSection = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section--hidden");
+
+  sectionObserver.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
